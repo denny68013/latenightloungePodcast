770b023d84100deabdb6f7eb3765cbf8d52e1eb8
denny68013 <denny68013@gmail.com>
2023-09-06 23:57:43 +0800

    first commit

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    domains: ["d3mww1g1pfq2pt.cloudfront.net"],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
